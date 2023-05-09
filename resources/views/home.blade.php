e13a22d200a920ef8b1ed38e66180a225ae89d39
niccov <niccoventuri27@gmail.com>
2023-05-09 18:41:42 +0200

    top-main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts/main-layout')
 
 @section('content')
-<main></main>
+<main>
+
+    <div id="container">
+       <div id="container-card">
+      
+         @foreach ($comics as $singleComic)
+             <div class="card">
+  
+               <div class="card-inner">
+                 <div class="container-img">
+                    <img src="{{$singleComic['thumb']}}" alt="immagine comic">
+                 </div>
+              
+                 <div>
+                    {{$singleComic['title']}}
+                 </div>
+               </div>
+             </div>
+         @endforeach
+         </div>
+      
+         <div id="container-button">
+             <button>LOAD MORE</button>
+         </div>
+    </div>
+
+    
+</main>
 @endsection
